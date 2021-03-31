9c1e6b6da2ce1b7a2a5afdebd450eaea81939fd7
maxlath <k@maxlath.eu>
2021-03-31 20:49:58 +0200

    Updating dependencies: update wikicite-extract

--- js/wikicite-extract.js
+++ js/wikicite-extract.js
@@ -1,25 +1,16 @@
 #!/usr/bin/env node
 
 const fs = require('fs')
-const split = require('split')
-const { parser, serializer, filter } = require('wikidata-filter')
+const { getEntitiesStream, filterFormatAndSerialize } = require('wikibase-dump-filter')
+const { isItemId } = require('wikibase-sdk')
 const itemFilter = require('./lib/item_filter')
 
-var classes = []
+const classes = fs.readFileSync(process.argv[2])
+  .toString()
+  .split('\n')
+  .filter(isItemId)
 
-fs.createReadStream(process.argv[2])
-  // read list of class qids from file
-  .pipe(split())
-  .on('data', (qid) => {
-    if (qid.match(/^Q[0-9]+$/)) {
-      classes.push(qid)
-    }
-  })
-  // filter and simplify instance items
-  .on('close', () => {
-    parser(process.stdin)
-    .filter(itemFilter(classes))
-    .filter(filter({'simplified': true}))
-    .filter(serializer)
-    .pipe(process.stdout)
-  })
+getEntitiesStream(process.stdin)
+.filterAndMap(itemFilter(classes))
+.filterAndMap(filterFormatAndSerialize({'simplified': true}))
+.pipe(process.stdout)
